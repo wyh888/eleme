aadd296a1c83a97799f5fa1f5523c37c5931fde2
Harvey <944847870@qq.com>
2017-08-16 22:25:57 +0800

    完成food组件

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-show="showFlag" class="food" transition="move">
+  <div v-show="showFlag" class="food" transition="move" v-el:food>
   	<div class="food-content">
   		<div class="image-header">
   			<img :src="food.image">
@@ -18,11 +18,19 @@
   				<span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
   			</div>
   		</div>
+  		<div class="cartcontrol-wrapper">
+  			<cartcontrol :food="food"></cartcontrol>
+  		</div>
+  		<div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0" transition="fade">加入购物车</div>
   	</div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+	import Vue from 'vue';
+	import BScroll from 'better-scroll';
+	import cartcontrol from 'components/cartcontrol/cartcontrol';
+
 	export default {
 		props: {
 			food: {
@@ -37,10 +45,29 @@
 		methods: {
 			show() {
 				this.showFlag = true;
+				this.$nextTick(() => {
+					if (!this.scroll) {
+						this.scroll = new BScroll(this.$els.food, {
+							click: true
+						});
+					} else {
+						this.scroll.refresh();
+					}
+				});
 			},
 			hide() {
 				this.showFlag = false;
+			},
+			addFirst(event) {
+				if (!event._constructed) {
+					return;
+				}
+				Vue.set(this.food, 'count', 1);
+				this.$dispatch('cart.add', event.target);
 			}
+		},
+		components: {
+			cartcontrol
 		}
 	};
 </script>
@@ -79,4 +106,55 @@
 					padding: 10px
 					font-size: 20px
 					color: #fff
+		.content
+			padding: 18px
+			.title
+				line-height: 14px
+				margin-bottom: 8px
+				font-size: 14px
+				font-weight: 700
+				color: rgb(7, 17, 27)
+			.detail
+				margin-bottom: 18px
+				line-height: 10px
+				height: 10px
+				font-size: 0
+				.sell-count, .rating 
+					font-size: 10px
+					color: rgb(147, 153, 159)
+				.sell-count
+					margin-right: 12px
+			.price
+				font-weight: 700
+				line-height: 24px
+				.now
+					margin-right: 8px
+					font-size: 14px
+					color: rgb(240, 20, 20)
+				.old
+					text-decoration: line-through
+					font-size: 10px
+					color: rgb(147, 153, 159)
+		.cartcontrol-wrapper
+			position: absolute
+			right: 12px
+			bottom: 12px
+		.buy
+			position: absolute
+			right: 18px
+			bottom: 18px
+			z-index: 10
+			height: 24px
+			line-height: 24px
+			padding: 0 12px
+			box-sizing: border-box
+			border-radius: 12px
+			font-size: 10px
+			color: #fff
+			background: rgb(0, 160, 220)
+			&.fade-transition
+				opacity: 1
+				transition: all 0.2s 
+			&.fade-enter, &.fade-leave
+				opacity: 0
 </style>
